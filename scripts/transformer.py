a768188e83c2daeb18d283cfea29ef6cddb3b5fd
Su Xian <suxian06@gmail.com>
2023-12-21 16:56:39 -0800

    finished transformer model

--- scripts/transformer.py
+++ scripts/transformer.py
@@ -6,6 +6,28 @@ Some codes bellow are borrowed (such as CustomSchedule class) from the
 tensorflow website: https://www.tensorflow.org/text/tutorials/transformer
 for reference purposes.
 """
+import os
+import numpy as np
+import pandas as pd
+import pickle
+import sklearn
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import layers
+import scipy.sparse as sparse
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+
+gpus = tf.config.list_physical_devices("GPU")
+tf.config.experimental.set_memory_growth(gpus[0], True)
+
+with open("C:/Data/Lab/PatEmbedding/eMERGE/model/50dim/\
+50_dim_embedding/namelist", "rb") as fp:   # Unpickling
+    namelist = pickle.load(fp)
+
+embd_data = np.load("C:/Data/Lab/PatEmbedding/eMERGE/model/50dim/\
+50_dim_embedding/embds_mean.npy")
 
 # custom learning rate, setup warmup steps
 # define loss function
@@ -50,10 +72,72 @@ def random_mask(data_batch,fraction = .2, MAX_LENGTH = 250):
     data_batch = [0 if i in set(mask_position) else data_batch[i] for i in range(len(data_batch))]
     return data_batch
 
+train_step_signature = [
+    tf.TensorSpec(shape=(None, None), dtype=tf.int32),
+    tf.TensorSpec(shape=(None, None), dtype=tf.int32),
+]
 
-### defining objects for the model
+# The `@tf.function` trace-compiles train_step into a TF graph for faster
+# execution. The function specializes to the precise shape of the argument
+# tensors. To avoid re-tracing due to the variable sequence lengths or variable
+# batch sizes (the last batch is smaller), use input_signature to specify
+# more generic shapes.
+
+@tf.function(input_signature=train_step_signature)
+def train_step(inp, tar):
+    # here inp is the masked-input, tar is the non-masked target
+
+    with tf.GradientTape() as tape:
+        predictions = transformer([inp, tar],
+                                     training = True)
+        loss = loss_function(tar, predictions)
+
+        gradients = tape.gradient(loss, transformer.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, transformer.trainable_variables))
+
+        train_loss(loss)
+        train_accuracy(accuracy_function(tar, predictions))
+
+def train(EPOCHS):
+
+    checkpoint_path = 'C:/Data/Lab/PatEmbedding/eMERGE/pat_embd/checkpoints/testrun/'
+
+    ckpt = tf.train.Checkpoint(transformer=transformer,
+                               optimizer=optimizer)
+
+    ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=1)
+
+    # If a checkpoint exists, restore the latest checkpoint.
+    if ckpt_manager.latest_checkpoint:
+        ckpt.restore(ckpt_manager.latest_checkpoint)
+        print('Latest checkpoint restored!!')
+
+    for epoch in range(EPOCHS):
+        train_loss.reset_states()
+        train_accuracy.reset_states()
+        # inp -> portuguese, tar -> english
+        for (batch,data) in enumerate(train_batches):
+            tar, inp = data[0], data[1]
+            train_step(inp, tar)
+
+            if batch % 200 == 0:
+                print(f'Epoch {epoch + 1} Batch \
+{batch} Loss {train_loss.result():.4f} Accuracy {train_accuracy.result():.4f}')
+
+        ckpt_save_path = ckpt_manager.save()
+
+    return None
+
 
 if __name__ == "__main__":
+
+    import multiprocessing
+    import logging
+    import time
+    import transformer_model as model
+    #import transformer_training_preprocess as preprocessing
+
+    # defining loss obect, metrics, and hyperparameters
     loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
         from_logits=True, reduction='none')
     train_loss = tf.keras.metrics.Mean(name='train_loss')
@@ -66,6 +150,7 @@ if __name__ == "__main__":
                                         beta_2=0.98,
                                         epsilon=1e-9)
 
+    # initiate the model
     transformer = model.Transformer(num_layers=6, # layer, L
         d_model=200, # embeddings of  feature, H
         num_heads=10, # numbers of head A
@@ -73,40 +158,15 @@ if __name__ == "__main__":
         input_vocab_size=len(namelist),
         embedding_matrix = np.concatenate((np.zeros(shape = (1,50)),embd_data)))
 
-    import multiprocessing
-    import logging
-    import time
+    #data_pair = create_pat_vocabulary_training()
+    #train_data = np.array([(x[0],random_mask(x[0])) for x in data_pair])
 
+    train_data = np.load("C:/Data/Lab/PatEmbedding/eMERGE/pat_embd/dataset/train_data.npy")
+    train_data_input = np.load("C:/Data/Lab/PatEmbedding/eMERGE/pat_embd/dataset/train_data_input.npy")
 
-    def train(EPOCHS=EPOCHS):
-
-        checkpoint_path = 'C:/Data/Lab/PatEmbedding/eMERGE/pat_embd/checkpoints/testrun/'
-
-        ckpt = tf.train.Checkpoint(transformer=transformer,
-                                   optimizer=optimizer)
-
-        ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=1)
-
-        # If a checkpoint exists, restore the latest checkpoint.
-        if ckpt_manager.latest_checkpoint:
-            ckpt.restore(ckpt_manager.latest_checkpoint)
-            print('Latest checkpoint restored!!')
-
-        for epoch in range(EPOCHS):
-            train_loss.reset_states()
-            train_accuracy.reset_states()
-            # inp -> portuguese, tar -> english
-            for (batch,data) in enumerate(train_batches):
-                tar, inp = data[0], data[1]
-                train_step(inp, tar)
-
-                if batch % 200 == 0:
-                    print(f'Epoch {epoch + 1} Batch \
-{batch} Loss {train_loss.result():.4f} Accuracy {train_accuracy.result():.4f}')
-
-            ckpt_save_path = ckpt_manager.save()
-
-        return None
+    train_batches = tf.data.Dataset.from_tensor_slices((train_data,train_data_input))
+    BATCH_SIZE = 32
+    train_batches = train_batches.batch(BATCH_SIZE)
 
     EPOCHS = 10
     p = multiprocessing.Process(target=train(EPOCHS = EPOCHS))
--- scripts/transformer_training_preprocess.py
+++ scripts/transformer_training_preprocess.py
@@ -3,23 +3,33 @@ This script contains code used for constructing the transformer model to perform
 the embedding for patient visits within a year.
 """
 
-import os
-
+# import os
+#
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import scipy.stats as sp
-import seaborn as sns
+import json
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import scipy.stats as sp
+# import seaborn as sns
+#
+# import sklearn
+# import tensorflow as tf
+# import matplotlib.pyplot as plt
+# import tensorflow.keras as keras
+# from tensorflow.keras.optimizers import Adam
+# from tensorflow.keras import layers
+# import scipy.sparse as sparse
+# from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import OneHotEncoder
+#
+# gpus = tf.config.list_physical_devices("GPU")
+# tf.config.experimental.set_memory_growth(gpus[0], True)
 
-import sklearn
-import tensorflow as tf
-import matplotlib.pyplot as plt
-import tensorflow.keras as keras
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras import layers
-import scipy.sparse as sparse
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
+# need patient level data
+# might be able to directly retrieve from Jarvik server
+
+with open("C:/Data/Lab/PatEmbedding/eMERGE/patient_vec_by_year.json") as f:
+    patient_vec = json.load(f)
 
 def create_code_vocabulary():
     embd_data = np.load("C:/Data/Lab/PatEmbedding/eMERGE/model/50dim/\
@@ -54,3 +64,12 @@ def create_pat_vocabulary_training(patient_vec = patient_vec):
             data_pair.append((collector[i],collector[i+1]))
 
     return data_pair
+
+def trime_training_data(train_data = train_data, train_data_input = train_data_input):
+    #let at least input has 5 codes
+    remove_codes = np.where([sum(train_data_input[i]) <=5 for i in range(len(train_data_input))])[0]
+    idx = list(set(list(range(len(train_data)))).difference(remove_codes))
+    train_data = train_data[idx]
+    train_data_input = train_data_input[idx]
+    del idx
+    return train_data, train_data_input
