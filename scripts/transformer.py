9f3b2c251679a15ab297602069c0614da209aaa1
Su Xian <suxian06@gmail.com>
2023-12-21 16:20:43 -0800

    fixed parameter bugs on the autoencoder

--- scripts/transformer.py
+++ scripts/transformer.py
@@ -9,7 +9,7 @@ for reference purposes.
 
 # custom learning rate, setup warmup steps
 # define loss function
-# now it is to mask some labels but restore the whole prediction (the goal changed, easier to implement)
+# mask some labels but restore the whole prediction
 class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
     def __init__(self, d_model, warmup_steps=500):
         super(CustomSchedule, self).__init__()
@@ -53,20 +53,62 @@ def random_mask(data_batch,fraction = .2, MAX_LENGTH = 250):
 
 ### defining objects for the model
 
-loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
-    from_logits=True, reduction='none')
-train_loss = tf.keras.metrics.Mean(name='train_loss')
-train_accuracy = tf.keras.metrics.Mean(name='train_accuracy')
+if __name__ == "__main__":
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True, reduction='none')
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+    train_accuracy = tf.keras.metrics.Mean(name='train_accuracy')
 
-d_model=50
-learning_rate = CustomSchedule(d_model)
-optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98,
-                                     #clipvalue = 5.0,
-                                     epsilon=1e-9)
+    d_model=50
+    learning_rate = CustomSchedule(d_model)
+    optimizer = tf.keras.optimizers.Adam(learning_rate,
+                                        beta_1=0.9,
+                                        beta_2=0.98,
+                                        epsilon=1e-9)
 
-transformer = model.Transformer(num_layers=6, # layer, L
-    d_model=200, # embeddings of  feature, H
-    num_heads=10, # numbers of head A
-    dff=2048,
-    input_vocab_size=len(namelist),
-    embedding_matrix = np.concatenate((np.zeros(shape = (1,50)),embd_data)))
+    transformer = model.Transformer(num_layers=6, # layer, L
+        d_model=200, # embeddings of  feature, H
+        num_heads=10, # numbers of head A
+        dff=2048,
+        input_vocab_size=len(namelist),
+        embedding_matrix = np.concatenate((np.zeros(shape = (1,50)),embd_data)))
+
+    import multiprocessing
+    import logging
+    import time
+
+
+    def train(EPOCHS=EPOCHS):
+
+        checkpoint_path = 'C:/Data/Lab/PatEmbedding/eMERGE/pat_embd/checkpoints/testrun/'
+
+        ckpt = tf.train.Checkpoint(transformer=transformer,
+                                   optimizer=optimizer)
+
+        ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=1)
+
+        # If a checkpoint exists, restore the latest checkpoint.
+        if ckpt_manager.latest_checkpoint:
+            ckpt.restore(ckpt_manager.latest_checkpoint)
+            print('Latest checkpoint restored!!')
+
+        for epoch in range(EPOCHS):
+            train_loss.reset_states()
+            train_accuracy.reset_states()
+            # inp -> portuguese, tar -> english
+            for (batch,data) in enumerate(train_batches):
+                tar, inp = data[0], data[1]
+                train_step(inp, tar)
+
+                if batch % 200 == 0:
+                    print(f'Epoch {epoch + 1} Batch \
+{batch} Loss {train_loss.result():.4f} Accuracy {train_accuracy.result():.4f}')
+
+            ckpt_save_path = ckpt_manager.save()
+
+        return None
+
+    EPOCHS = 10
+    p = multiprocessing.Process(target=train(EPOCHS = EPOCHS))
+    p.start()
+    p.join()
